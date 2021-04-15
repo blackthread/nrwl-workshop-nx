975a59f869147ba9f4ceef5e69a2160f2e8b79fe
steve keyen <stevekeyen@gmail.com>
2021-04-15 21:38:02 +0200

    added boundaries , eslint.json etc

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
+import { Game } from '@bg-hoard/util-interface';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -14,7 +15,7 @@ export class AppComponent {
   }
 
   title = 'Board Game Hoard';
-  games = this.http.get<any[]>('/api/games');
+  games = this.http.get<Game[]>('/api/games');
   formatRating = formatRating;
 }
 
